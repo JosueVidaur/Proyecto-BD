7efc61a4791ad999be388d0b3eee19be0b575b14
RonyVidaur <rony.vidaur@gmail.com>
2018-12-05 21:07:25 -0600

    cleaned functions

--- api/app.js
+++ api/app.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
 
 // probando conexion
-var oracledb = require('oracledb');
+var oracledb = require("oracledb");
 oracledb.getConnection(
   {
-    user          : "JOVS",
-    password      : "oracle",
-    connectString : "localhost/XE"
+    user: "JOVS",
+    password: "oracle",
+    connectString: "localhost/XE"
   },
-  function(err, connection)
-  {
-    if (err) { console.error(err); return; }
-    connection.execute(
-      "SELECT * FROM TBL_USUARIOS",
-      function(err, result)
-      {
-        if (err) { console.error(err); return; }
-        console.log(result.rows);
-      });
-  });
-
-
+  function(err, connection) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    connection.execute("SELECT * FROM TBL_USUARIOS", function(err, result) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(result.rows);
+    });
+  }
+);
 
 const app = express();
 app.use(bodyParser.json());
@@ -33,7 +34,6 @@ app.use(express.static("./aplicativo"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/aplicativo/index.html"));
 });
-
 
 require("./server/routes/index")(app);
 module.exports = app;
--- api/server/routes/index.js
+++ api/server/routes/index.js
@@ -1,8 +1,8 @@
 module.exports = app => {
   app.post("/login", (req, res) => {
-    const {username, password } = req.body;
+    const { username, password } = req.body;
     res.status(200).send({
-      message: '*****'
-    })
+      message: "*****"
+    });
   });
 };
